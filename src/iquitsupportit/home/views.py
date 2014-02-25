563fe64eac031b3c5ccd3766127ce2f4ef0bb75e
Francois Gaudin <contact@francoisgaudin.com>
2014-02-24 23:20:39 -0800

    Finished first draft of home page

--- src/iquitsupportit/home/views.py
+++ src/iquitsupportit/home/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from quitter.models import Profile
 from django.conf import settings
-from quitter.forms import SignupForm
+from quitter.forms import SignupForm, LoginForm
 
 
 def index(request):
     context = {
         'featured': Profile.objects.get(slug=settings.DEFAULT_PROFILE),
-        'signup_form': SignupForm()
+        'signup_form': SignupForm(),
+        'login_form': LoginForm()
     }
     return render_to_response('home/index.html',
                               context,
