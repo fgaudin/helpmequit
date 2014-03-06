300064421358f3283f75c76018c4218b6940ffe6
Francois Gaudin <contact@francoisgaudin.com>
2014-03-05 23:40:17 -0800

    Fixing wsgi for gandi

--- src/iquitsupportit/iquitsupportit/wsgi.py
+++ src/iquitsupportit/iquitsupportit/wsgi.py
@@ -8,6 +8,11 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                             'src',
+                             'iquitsupportit'))
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iquitsupportit.settings_prod")
 
 from django.core.wsgi import get_wsgi_application
