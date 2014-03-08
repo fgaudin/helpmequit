8afb5adc93e35dd6cc2e3e795cb9be559083f303
Francois Gaudin <contact@francoisgaudin.com>
2014-03-07 18:02:34 -0800

    Conf for gondor

--- iquitsupportit/iquitsupportit/settings_prod.py
+++ iquitsupportit/iquitsupportit/settings_prod.py
@@ -1,4 +1,5 @@
 from iquitsupportit.settings import *
+import urlparse
 
 DEBUG = False
 TEMPLATE_DEBUG = False
@@ -8,16 +9,21 @@ ALLOWED_HOSTS = ['.helpmequ.it',
                  '.url-de-test.ws'
                  ]
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'la_base_des_dons',
-        'USER': 'le_plus_fumeur',
-        'PASSWORD': 'yoighImAmNojLetFagCisulp',
-        'HOST': '127.0.0.1',
-        'PORT': '',
+if "GONDOR_DATABASE_URL" in os.environ:
+    urlparse.uses_netloc.append("postgres")
+    url = urlparse.urlparse(os.environ["GONDOR_DATABASE_URL"])
+    DATABASES = {
+        "default": {
+            "ENGINE": {
+                "postgres": "django.db.backends.postgresql_psycopg2"
+            }[url.scheme],
+            "NAME": url.path[1:],
+            "USER": url.username,
+            "PASSWORD": url.password,
+            "HOST": url.hostname,
+            "PORT": url.port
+        }
     }
-}
 
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
@@ -26,3 +32,38 @@ EMAIL_HOST_PASSWORD = 'bluk0jrekIsjefEldIdf3fop'
 EMAIL_USE_TLS = True
 
 AWS_STORAGE_BUCKET_NAME = 'helpmequit-users'
+
+MEDIA_ROOT = os.path.join(os.environ["GONDOR_DATA_DIR"], "site_media", "media")
+STATIC_ROOT = os.path.join(os.environ["GONDOR_DATA_DIR"], "site_media", "static")
+
+MEDIA_URL = "/site_media/media/"  # make sure this maps inside of site_media_url
+STATIC_URL = "/site_media/static/"  # make sure this maps inside of site_media_url
+ADMIN_MEDIA_PREFIX = STATIC_URL + "admin/"
+
+FILE_UPLOAD_PERMISSIONS = 0640
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "simple": {
+            "format": "%(levelname)s %(message)s"
+        },
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "simple"
+        }
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+    "loggers": {
+        "django.request": {
+            "propagate": True,
+        },
+    }
+}
--- wsgi.py
+++
@@ -1,1 +0,0 @@
-iquitsupportit/iquitsupportit/wsgi.py
