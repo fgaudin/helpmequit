fe918beb81b3a39a1e6bd790ff8a034637985eb0
Francois Gaudin <contact@francoisgaudin.com>
2014-03-09 17:12:12 -0700

    Added i18n url to the home page

--- src/iquitsupportit/urls.py
+++ src/iquitsupportit/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 admin.autodiscover()
 
+
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'home.views.index', name='home'),
     url(r'^signup/(?P<hash>[0-9a-f\-]+)/', 'quitter.views.confirm_signup', name="confirm_signup"),
     url(r'^signup/', 'quitter.views.signup', name="signup"),
     url(r'^login/', 'quitter.views.login', name="login"),
@@ -17,4 +17,8 @@ urlpatterns = patterns('',
     url(r'^b/(?P<id>\d+)/form/$', 'quitter.views.beneficiary_form', name='beneficiary_form'),
     url(r'^p/', include('supporter.urls')),
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += i18n_patterns('',
+    url(r'^$', 'home.views.index', name='home'),
 )
